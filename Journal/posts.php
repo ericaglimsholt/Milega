39766330569ae53e4b8edff7b92befe84cde7477
Erica  Glimsholt <ericaglimsholt@gmail.com>
2016-11-10 13:17:43 +0100

    Copied all from index to posts

--- Journal/posts.php
+++ Journal/posts.php
@@ -1,1 +1,16 @@
-<?php 
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Reflection journal</title>
+  </head>
+  <body>
+<h1>Write something and we will save it</h1>
+
+    <form action="post.php" method="POST">
+      Rubrik:<input type="text" name="title" value=""> </br>
+      Innehåll:<input type="text" name="content" value=""> </br>
+      <input type="submit" name="submit" value="Skicka">
+    </form>
+
+  </body>
+</html>
