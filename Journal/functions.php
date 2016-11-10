97de0bf832a8a614959a19b03c29de7be38d0f5e
Erica  Glimsholt <ericaglimsholt@gmail.com>
2016-11-10 14:58:24 +0100

    Deleted session_start because not needed

--- Journal/functions.php
+++ Journal/functions.php
@@ -4,13 +4,11 @@ require("database.php");
 
 function registerPost($connection, $title, $content)
 {
-  session_start();
   if (!dbPost($connection, "INSERT INTO reflection (postContent, postTitle) VALUES ('$content', '$title')")) {
     $_SESSION["error"] = "Vi kunde inte spara ditt inlägg.";
     return false;
   } else {
     $_SESSION["message"] = "Vi lyckades spara ditt inlägg!";
     return true;
-    die();
   }
 }
