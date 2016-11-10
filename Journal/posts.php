67d8d4242ffdcbb8862fee451471a07cc9ffe39b
Erica  Glimsholt <ericaglimsholt@gmail.com>
2016-11-10 13:23:05 +0100

    Included an error message

--- Journal/posts.php
+++ Journal/posts.php
@@ -4,7 +4,16 @@
     <title>Reflection journal</title>
   </head>
   <body>
-<h1>Write something and we will save it</h1>
+
+<?php
+// Error message
+if ($error) {
+      print '<h4 style="color:red; font-weight:bold;">'.$error.'</h4>';
+      unset($_SESSION["error"]);
+    }
+?>
+
+    <h1>Write something and we will save it</h1>
 
     <form action="post.php" method="POST">
       Rubrik:<input type="text" name="title" value=""> </br>
