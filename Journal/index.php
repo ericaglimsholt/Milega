f9b1492ce82a5bff5cae9cc2cfb6f0163d348f92
Erica  Glimsholt <ericaglimsholt@gmail.com>
2016-11-10 14:53:16 +0100

    Changes posts to index

--- Journal/index.php
+++ Journal/index.php
@@ -9,7 +9,23 @@
 
 require("functions.php");
 
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  if ($_POST["title"] !== "" && $_POST["content"] !== "") {
+    registerPost($connection, $_POST["title"], $_POST["content"]);
+  } else {
+    session_start();
+    $_SESSION["error"] = "Du har glömt att skriva rubrik eller innehåll.";
+    header("Location: /");
+    die();
+
+  }
+}
+
 // Error message
+
+$error = $_SESSION["error"] ?? "";
+$message = $_SESSION["message"] ?? "";
+
     if ($error) {
       print '<h4 style="color:red; font-weight:bold;">'.$error.'</h4>';
       unset($_SESSION["error"]);
@@ -17,18 +33,20 @@ require("functions.php");
 
 // Message
     if ($message) {
-      print '<h4 style="color:red; font-weight:bold;">'.$message.'</h4>';
+      print '<h4 style="color:green; font-weight:bold;">'.$message.'</h4>';
       unset($_SESSION["message"]);
     }
 ?>
 
     <h1>Write something and we will save it</h1>
 
-    <form action="post.php" method="POST">
+    <form action="<?php $_SERVER['PHP_SELF']?>" method="POST">
       Rubrik:<input type="text" name="title" value=""> </br>
       Innehåll:<input type="text" name="content" value=""> </br>
       <input type="submit" name="submit" value="Skicka">
     </form>
+
+
 
   </body>
 </html>
