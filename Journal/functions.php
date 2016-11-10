0cdb672a883235b27d4ae959d890c79b9029589a
Erica  Glimsholt <ericaglimsholt@gmail.com>
2016-11-10 14:53:57 +0100

    Added the dbPost function and ended the session

--- Journal/functions.php
+++ Journal/functions.php
@@ -5,11 +5,12 @@ require("database.php");
 function registerPost($connection, $title, $content)
 {
   session_start();
-  if (!$connection, "INSERT INTO reflection (postContent, postTitle) VALUES ('$content', '$title')") {
+  if (!dbPost($connection, "INSERT INTO reflection (postContent, postTitle) VALUES ('$content', '$title')")) {
     $_SESSION["error"] = "Vi kunde inte spara ditt inlägg.";
     return false;
   } else {
     $_SESSION["message"] = "Vi lyckades spara ditt inlägg!";
     return true;
+    die();
   }
 }
