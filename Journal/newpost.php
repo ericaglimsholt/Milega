1eeab4dca75d1cb4f63267c75154380834a10b2e
Erica  Glimsholt <ericaglimsholt@gmail.com>
2016-11-10 14:53:31 +0100

    Created a header back to root

--- Journal/newpost.php
+++ Journal/newpost.php
@@ -3,11 +3,13 @@
 require("functions.php");
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-  if isset($_POST["title"] && $_POST["content"]) {
+  if ($_POST["title"] !== "" && $_POST["content"] !== "") {
     registerPost($connection, $_POST["title"], $_POST["content"]);
   } else {
     session_start();
-    $_SESSION["error"] = "Du har glömt att skriva rubrik eller innehåll."
+    $_SESSION["error"] = "Du har glömt att skriva rubrik eller innehåll.";
+    header("Location: /");
     die();
+
   }
 }
