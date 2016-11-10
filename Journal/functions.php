bae36f287b04272d9be8ad0d47ce65fc560ef7b7
Erica  Glimsholt <ericaglimsholt@gmail.com>
2016-11-10 13:43:58 +0100

    Created a function that put the post into the database

--- Journal/functions.php
+++ Journal/functions.php
@@ -1,3 +1,15 @@
 <?php
 
 require("database.php");
+
+function registerPost($connection, $title, $content)
+{
+  session_start();
+  if (!$connection, "INSERT INTO reflection (postContent, postTitle) VALUES ('$content', '$title')") {
+    $_SESSION["error"] = "Vi kunde inte spara ditt inlägg.";
+    return false;
+  } else {
+    $_SESSION["message"] = "Vi lyckades spara ditt inlägg!";
+    return true;
+  }
+}
