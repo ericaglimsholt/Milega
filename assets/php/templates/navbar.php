76d8cca36a88915e130884ccf68304a87a8ad746
Erica  Glimsholt <ericaglimsholt@gmail.com>
2016-11-16 13:42:45 +0100

    Created the path for the home button to root

--- assets/php/templates/navbar.php
+++ assets/php/templates/navbar.php
@@ -1,10 +1,8 @@
 <!--NAVBAR-->
 
-<nav>
 <ul>
 	<li><a href="#">Kursmaterial</a></li>
 	<li><a href="reflection.php">Reflektioner</a></li>
 	<li><a href="goals.php">Mål</a></li>
-	<li><a href="#">Hem</a></li>
+	<li><a href="Location: /">Hem</a></li>
 </ul>
-</nav>
