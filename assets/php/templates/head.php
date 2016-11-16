8715b5236ecf132b76adc57d9fa66c3561ff68e5
Erica  Glimsholt <ericaglimsholt@gmail.com>
2016-11-16 13:34:37 +0100

    Included meta tags

--- assets/php/templates/head.php
+++ assets/php/templates/head.php
@@ -1,15 +1,6 @@
 
 <meta charset="utf-8">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
-
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <link rel="stylesheet" type="text/css" href="assets/style/style.css">
-
-<title></title>
-
-<!--NAVBAR-->
-<ul>
-	<li><a href="#">Kursmaterial</a></li>
-	<li><a href="reflection.php">Reflektioner</a></li>
-	<li><a href="goals.php">Mål</a></li>
-	<li><a href="#">Hem</a></li>
-</ul>
+<title>Milega</title>
