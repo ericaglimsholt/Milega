34e6b71c190d23a4989257a1f799be1168cd4486
Erica  Glimsholt <ericaglimsholt@gmail.com>
2016-11-16 15:09:58 +0100

    Included the first lecture

---
+++ lectures.php
@@ -0,0 +1,63 @@
+
+<html>
+  <head>
+
+    <?php
+			require('assets/php/templates/head.php');
+		?>
+
+  </head>
+  <body>
+
+
+
+
+    <div class="content">
+    <div class="left">
+
+
+
+      <ul>
+        <li style="float:left;"><a href="#">Logotyp :)</a></li>
+      </ul>
+
+
+      <h1>Kursmaterial</h1>
+
+      <div id="goalcontent">
+
+        <h2>Mina styrkor och utvecklingsområden</h2>
+        <h2>Ledarkompetenshjulet</h2>
+        <h2>Chefens roller</h2>
+      </div>
+
+    </div>
+
+    <div class="right">
+
+      <?php
+        require('assets/php/templates/navbar.php');
+      ?>
+
+			  <div id="reflectioncontent">
+        <h3>Mina styrkor och utvecklingsområden</h3>
+        <p> Detta är första övningen du gör i att färdigställa målet med ditt ledarskap. Dokumentet kommer att vara levande under hela utbildningen och det är meningen att du skall fylla på allteftersom du tillför mer kunskaper om dig själv och vad du vill göra i din roll som ledare.
+        Dag 7 kommer du att inför gruppen kort få redogöra för hur långt ditt arbete med detta kommit.
+         
+        Denna första övning är uppdelad i tre olika moment. I den första individuella övningen reflekterar du över din JTI. I den andra individuella övningen kartlägger du dig själv utifrån ditt uppdrag.
+
+        Därefter skall du i del 3 dels arbeta med en individuell kompetensprofil  samt – i  anslutning till denna - träffa en person i gruppen som skall ”coacha” dig så att du får feedback på dina specifika kompetensområden. </p>
+
+
+
+
+			</div>
+
+
+    </div>
+
+    </div>
+  </div>
+
+  </body>
+</html>
