9be4aa26c01bdd2b6366076b46239defef632a89
Erica  Glimsholt <ericaglimsholt@gmail.com>
2016-11-17 11:17:18 +0100

    Fixed the navigation

--- assets/php/templates/header.php
+++ assets/php/templates/header.php
@@ -7,6 +7,6 @@
 		<li><a href="lectures.php">Kursmaterial</a></li>
 		<li><a href="reflection.php">Reflektioner</a></li>
 		<li><a href="goals.php">Mål</a></li>
-		<li><a href="index.php">Hem</a></li>
+		<li><a href="index.php">Nytt mål</a></li>
 	</ul>
 </nav>
