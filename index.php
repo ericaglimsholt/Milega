c774a1ebc790ed78fc1bf38149b133c377041ec0
Erica  Glimsholt <ericaglimsholt@gmail.com>
2016-11-17 14:15:08 +0100

    Deleted weird syntax

--- index.php
+++ index.php
@@ -36,11 +36,10 @@
 
 			<div id="reflectioncontent">
 
-<<<<<<< HEAD
+
 				<div id="chart" class="ct-chart ct-perfect-fourth"></div>
-=======
+
 				<img src="../../images/piechart.png" id="piechart" />
->>>>>>> 8fb4cdd31973f6135de54c20dca0a2f6e5505003
 
 
 
@@ -51,15 +50,15 @@
 
     </div>
   </div>
-<<<<<<< HEAD
+
   <script src="/assets/js/bower_components/chartist/dist/chartist.min.js"></script>
-=======
+
 <footer>
   <?php
     require('assets/php/templates/footer.php');
   ?>
 </footer>
 
->>>>>>> 8fb4cdd31973f6135de54c20dca0a2f6e5505003
+
   </body>
 </html>
