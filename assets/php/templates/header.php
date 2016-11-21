9525b670499727c6c92241a8387bc2bea9692e1f
Erica  Glimsholt <ericaglimsholt@gmail.com>
2016-11-21 11:30:58 +0100

    Included icons to menu

--- assets/php/templates/header.php
+++ assets/php/templates/header.php
@@ -3,11 +3,11 @@
 
 <nav>
 	<ul>
-		<li style="float:left;"><a href="#">Logotyp :)</a></li>
-		<li><a href="lectures.php">Kursmaterial</a></li>
-		<li><a href="reflection.php">Reflektioner</a></li>
-		<li><a href="goal.php">Mål</a></li>
-		<li><a href="goals.php">Nytt mål</a></li>
-		<li><a href="index.php">Hem</a></li>
+		<li style="float:left;"><a href="index.php">Logotyp :)</a></li>
+
+		<li><a href="lectures.php"><img src="../images/kursIcon.png" alt="" /> Kursmaterial</a></li>
+		<li><a href="reflection.php"><img src="../images/arkivIcon.png" alt="" /> Arkiv</a></li>
+		<li><a href="goal.php"><img src="../images/goalIcon.png" alt="" /> Mål</a></li>
+		<li><a href="goals.php"><img src="../images/PlusIcon.png" alt="" /> Nytt mål</a></li>
 	</ul>
 </nav>
