e0dcf0fc316f04cbe7a13a87a2abb6856335509a
Erica  Glimsholt <ericaglimsholt@gmail.com>
2016-11-21 11:41:51 +0100

    Updated the fonts and links in the footer

--- assets/php/templates/footer.php
+++ assets/php/templates/footer.php
@@ -2,7 +2,7 @@
 	<ul>
 		<li style="float:left;"><a href="#">Copyright Milega</a></li>
 		<li><a href="#">Logga ut</a></li>
-		<li><a href="#">Kontakt</a></li>
-		<li><a href="#">FAQ</a></li>
+		<li><a href="http://www.milega.se/kontakt">Kontakt</a></li>
+		<li><a href="http://www.milega.se/om-milega">FAQ</a></li>
 	</ul>
 </nav>
--- assets/php/templates/head.php
+++ assets/php/templates/head.php
@@ -4,6 +4,7 @@
 <meta charset="utf-8">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <link href="https://fonts.googleapis.com/css?family=Rubik:400,500" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <link rel="stylesheet" href="/assets/js/bower_components/chartist/dist/chartist.min.css">
 <link rel="stylesheet" type="text/css" href="assets/style/style.css">
