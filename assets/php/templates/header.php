45e432e42f39c7efb16c5279d7cc6c9e9f13be17
Erica  Glimsholt <ericaglimsholt@gmail.com>
2016-11-22 13:24:42 +0100

    Changed the icons to sag

--- assets/php/templates/header.php
+++ assets/php/templates/header.php
@@ -1,15 +1,16 @@
 <!--NAVBAR-->
 
 
+
 <nav>
 	<ul>
 
-		<li style="float:left;"><a href="index.php">Logotyp :)</a></li>
+		<li style="float:left;"><a href="index.php"><img src="../images/logo.png" /></a></li>
 
-		<li><a href="lectures.php"><img src="../images/kursIcon.png" /> Kursmaterial</a></li>
-		<li><a href="reflection.php"><img src="../images/arkivIcon.png" alt="" /> Arkiv</a></li>
-		<li><a href="goal.php"><img src="../images/goalIcon.png" alt="" /> Mål</a></li>
-		<li><a href="goals.php"><img src="../images/PlusIcon.png" alt="" /> Nytt mål</a></li>
+		<li><a href="lectures.php"><img src="../images/kursIcon.svg" /> Kursmaterial</a></li>
+		<li><a href="reflection.php"><img src="../images/arkivIcon.svg" alt="" /> Arkiv</a></li>
+		<li><a href="goal.php"><img src="../images/goalIcon.svg" alt="" /> Mål</a></li>
+		<li><a href="newgoal.php" class="new" ><img src="../images/PlusIcon.svg" alt="" /> Nytt mål</a></li>
 
 	</ul>
 </nav>
